796226ac74ab5d982f6b5b14b7cee4dee35a0a1a
devSLAVUS <91405914+devSLAVUS@users.noreply.github.com>
2021-10-04 16:54:02 +0300

    Update btc_bot.py

--- btc_bot.py
+++ btc_bot.py
@@ -4,12 +4,7 @@ from datetime import datetime
 import telebot
 from auth_data import token
 from telebot import types
-def get_data():
-    r = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
-    r = requests.get(r)
-    data = r.json()
-    price = data["price"]
-    print(f"{datetime.now().strftime('%d-%m-%Y     %H:%M')}\nSell BTC price:{price}")
+
 
 def telegram_bot(token):
     bot = telebot.TeleBot(token)
@@ -23,6 +18,30 @@ def telegram_bot(token):
         bot.send_message(message.chat.id, "Greetings, investor {0.first_name}!".format(message.from_user), reply_markup = markup)
     @bot.message_handler(content_types=["text"])
     def send_text(message):
+        def get_all(znak):
+            vesi = f"https://horo.mail.ru/prediction/{znak}/today/"
+            request_vesi = requests.get(vesi)
+            soup = BeautifulSoup(request_vesi.text, "lxml")
+            giro = soup.find("div", class_ = "article__item article__item_alignment_left article__item_html")
+            return giro
+
+        def get_weather(gorod):
+            r = f"http://api.openweathermap.org/data/2.5/weather?q={gorod}&appid=12a7eeeda8c4becc9503870405c37f9e&units=metric"
+            r = requests.get(r)
+            data = r.json() 
+            city = data["name"]
+            cur_weather = data["main"]["temp"]
+            weather_description = data["weather"][0]["main"]
+            humidity = data["main"]["humidity"]
+            pressure = data["main"]["pressure"]
+            wind = data["wind"]["speed"]
+            wd = "Посмотри в окно, не пойму что там за погода!"
+            sunrise_timestamp = datetime.fromtimestamp(data["sys"]["sunrise"])
+            sunset_timestamp = datetime.fromtimestamp(data["sys"]["sunset"])
+            length_of_the_day = datetime.fromtimestamp(data["sys"]["sunset"]) - datetime.fromtimestamp(data["sys"]["sunrise"])
+            open_weather = [city, cur_weather, weather_description, humidity, pressure, wind, sunrise_timestamp, sunset_timestamp, length_of_the_day]
+            return open_weather
+  
         if message.chat.type == "private":
             if message.text == 'Курс биткоина':
                 r = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
@@ -58,135 +77,78 @@ def telegram_bot(token):
                 markup.add(bitcoin, goroscope, pogoda)
                 bot.send_message(message.chat.id, "Greetings, investor {0.first_name}!".format(message.from_user), reply_markup = markup)
             elif message.text == 'Весы':
-                vesi = "https://horo.mail.ru/prediction/libra/today/"
-                request_vesi = requests.get(vesi)
-
-                soup = BeautifulSoup(request_vesi.text, "lxml")
-                page_p = soup.find("div", class_ = "article__item article__item_alignment_left article__item_html")
+                zn = "libra"
+                s = get_all(zn)         
                 bot.send_message(
-                    message.chat.id, f"{str(page_p.text)}"
+                    message.chat.id, f"{str(s.text)}"
                     )
             elif message.text == 'Рыбы':
-                fish = "https://horo.mail.ru/prediction/pisces/today/"
-                request_fish = requests.get(fish)
-
-                soup = BeautifulSoup(request_fish.text, "lxml")
-                page_p = soup.find("div", class_ = "article__item article__item_alignment_left article__item_html")
+                zn = "pisces"
+                s = get_all(zn)         
                 bot.send_message(
-                    message.chat.id, f"{str(page_p.text)}"
+                    message.chat.id, f"{str(s.text)}"
                     )
             elif message.text == 'Овен':
-                oven = "https://horo.mail.ru/prediction/aries/today/"
-                request_oven = requests.get(oven)
-
-                soup = BeautifulSoup(request_oven.text, "lxml")
-                page_p = soup.find("div", class_ = "article__item article__item_alignment_left article__item_html")
+                zn = "aries"
+                s = get_all(zn)         
                 bot.send_message(
-                    message.chat.id, f"{str(page_p.text)}"
+                    message.chat.id, f"{str(s.text)}"
                     )
             elif message.text == 'Телец':
-                telec = "https://horo.mail.ru/prediction/taurus/today/"
-                request_telec = requests.get(telec)
-
-                soup = BeautifulSoup(request_telec.text, "lxml")
-                page_p = soup.find("div", class_ = "article__item article__item_alignment_left article__item_html")
+                zn = "taurus"
+                s = get_all(zn)         
                 bot.send_message(
-                    message.chat.id, f"{str(page_p.text)}"
+                    message.chat.id, f"{str(s.text)}"
                     )
             elif message.text == 'Близнецы':
-                fish = "https://horo.mail.ru/prediction/gemini/today/"
-                request_fish = requests.get(fish)
-
-                soup = BeautifulSoup(request_fish.text, "lxml")
-                page_p = soup.find("div", class_ = "article__item article__item_alignment_left article__item_html")
+                zn = "gemini"
+                s = get_all(zn)         
                 bot.send_message(
-                    message.chat.id, f"{str(page_p.text)}"
+                    message.chat.id, f"{str(s.text)}"
                     )
             elif message.text == 'Рак':
-                fish = "https://horo.mail.ru/prediction/cancer/today/"
-                request_fish = requests.get(fish)
-
-                soup = BeautifulSoup(request_fish.text, "lxml")
-                page_p = soup.find("div", class_ = "article__item article__item_alignment_left article__item_html")
+                zn = "cancer"
+                s = get_all(zn)         
                 bot.send_message(
-                    message.chat.id, f"{str(page_p.text)}"
+                    message.chat.id, f"{str(s.text)}"
                     )
             elif message.text == 'Лев':
-                fish = "https://horo.mail.ru/prediction/leo/today/"
-                request_fish = requests.get(fish)
-
-                soup = BeautifulSoup(request_fish.text, "lxml")
-                page_p = soup.find("div", class_ = "article__item article__item_alignment_left article__item_html")
+                zn = "leo"
+                s = get_all(zn)         
                 bot.send_message(
-                    message.chat.id, f"{str(page_p.text)}"
+                    message.chat.id, f"{str(s.text)}"
                     )
             elif message.text == 'Дева':
-                fish = "https://horo.mail.ru/prediction/virgo/today/"
-                request_fish = requests.get(fish)
-
-                soup = BeautifulSoup(request_fish.text, "lxml")
-                page_p = soup.find("div", class_ = "article__item article__item_alignment_left article__item_html")
+                zn = "virgo"
+                s = get_all(zn)         
                 bot.send_message(
-                    message.chat.id, f"{str(page_p.text)}"
+                    message.chat.id, f"{str(s.text)}"
                     )
             elif message.text == 'Скорпион':
-                fish = "https://horo.mail.ru/prediction/scorpio/today/"
-                request_fish = requests.get(fish)
-
-                soup = BeautifulSoup(request_fish.text, "lxml")
-                page_p = soup.find("div", class_ = "article__item article__item_alignment_left article__item_html")
+                zn = "scorpio"
+                s = get_all(zn)         
                 bot.send_message(
-                    message.chat.id, f"{str(page_p.text)}"
+                    message.chat.id, f"{str(s.text)}"
                     )
             elif message.text == 'Стрелец':
-                fish = "https://horo.mail.ru/prediction/sagittarius/today/"
-                request_fish = requests.get(fish)
-
-                soup = BeautifulSoup(request_fish.text, "lxml")
-                page_p = soup.find("div", class_ = "article__item article__item_alignment_left article__item_html")
+                zn = "sagittarius"
+                s = get_all(zn)         
                 bot.send_message(
-                    message.chat.id, f"{str(page_p.text)}"
+                    message.chat.id, f"{str(s.text)}"
                     )
             elif message.text == 'Козерог':
-                fish = "https://horo.mail.ru/prediction/capricorn/today/"
-                request_fish = requests.get(fish)
-
-                soup = BeautifulSoup(request_fish.text, "lxml")
-                page_p = soup.find("div", class_ = "article__item article__item_alignment_left article__item_html")
+                zn = "capricorn"
+                s = get_all(zn)         
                 bot.send_message(
-                    message.chat.id, f"{str(page_p.text)}"
+                    message.chat.id, f"{str(s.text)}"
                     )
             elif message.text == 'Водолей':
-                fish = "https://horo.mail.ru/prediction/aquarius/today/"
-                request_fish = requests.get(fish)
-
-                soup = BeautifulSoup(request_fish.text, "lxml")
-                page_p = soup.find("div", class_ = "article__item article__item_alignment_left article__item_html")
+                zn = "aquarius"
+                s = get_all(zn)         
                 bot.send_message(
-                    message.chat.id, f"{str(page_p.text)}"
+                    message.chat.id, f"{str(s.text)}"
                     )
-            elif message.text == 'Москва':
-                r = "http://api.openweathermap.org/data/2.5/weather?q=moscow&appid=12a7eeeda8c4becc9503870405c37f9e&units=metric"
-                r = requests.get(r)
-                data = r.json()
-                city = data["name"]
-                cur_weather = data["main"]["temp"]
-                weather_description = data["weather"][0]["main"]
-                humidity = data["main"]["humidity"]
-                pressure = data["main"]["pressure"]
-                wind = data["wind"]["speed"]
-                wd = "Посмотри в окно, не пойму что там за погода!"
-                sunrise_timestamp = datetime.fromtimestamp(data["sys"]["sunrise"])
-                sunset_timestamp = datetime.fromtimestamp(data["sys"]["sunset"])
-                length_of_the_day = datetime.fromtimestamp(data["sys"]["sunset"]) - datetime.fromtimestamp(
-                data["sys"]["sunrise"])
-                bot.send_message(
-                    message.chat.id, f"***{datetime.now().strftime('%Y-%m-%d %H:%M')}***\n"
-                                    f"Погода в городе: Москва\nТемпература: {cur_weather}C°\n"
-                                    f"Влажность: {humidity}%\nДавление: {pressure} мм.рт.ст\nВетер: {wind} м/с\n"
-                                    f"Восход солнца: {sunrise_timestamp}\nЗакат солнца: {sunset_timestamp}\nПродолжительность дня: {length_of_the_day}\n"
-                                    f"Хорошего дня!"
-                                )
+                    
             elif message.text == 'Погода':
                 markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
                 msk = types.KeyboardButton('Москва')
@@ -196,72 +158,50 @@ def telegram_bot(token):
                 nazad = types.KeyboardButton('Назад')
                 markup.add(msk, spb, kur, dubai, nazad)
                 bot.send_message(message.chat.id,'Погода', reply_markup = markup)
-            elif message.text == 'Санкт-Петербург':
-                r = "http://api.openweathermap.org/data/2.5/weather?id=498817&appid=12a7eeeda8c4becc9503870405c37f9e&units=metric"
-                r = requests.get(r)
-                data = r.json()
-                city = data["name"]
-                cur_weather = data["main"]["temp"]
-                weather_description = data["weather"][0]["main"]
-                humidity = data["main"]["humidity"]
-                pressure = data["main"]["pressure"]
-                wind = data["wind"]["speed"]
-                wd = "Посмотри в окно, не пойму что там за погода!"
-                sunrise_timestamp = datetime.fromtimestamp(data["sys"]["sunrise"])
-                sunset_timestamp = datetime.fromtimestamp(data["sys"]["sunset"])
-                length_of_the_day = datetime.fromtimestamp(data["sys"]["sunset"]) - datetime.fromtimestamp(
-                data["sys"]["sunrise"])
+
+            elif message.text == 'Москва':
+                g = "Moscow"
+                s = get_weather(g)
                 bot.send_message(
                     message.chat.id, f"***{datetime.now().strftime('%Y-%m-%d %H:%M')}***\n"
-                                    f"Погода в городе: Санкт-Петербург\nТемпература: {cur_weather}C°\n"
-                                    f"Влажность: {humidity}%\nДавление: {pressure} мм.рт.ст\nВетер: {wind} м/с\n"
-                                    f"Восход солнца: {sunrise_timestamp}\nЗакат солнца: {sunset_timestamp}\nПродолжительность дня: {length_of_the_day}\n"
+                                    f"Погода в городе: Москва\nТемпература: {s[1]}C°\n"
+                                    f"Влажность: {s[3]}%\nДавление: {s[4]} мм.рт.ст\nВетер: {s[5]} м/с\n"
+                                    f"Восход солнца: {s[6]}\nЗакат солнца: {s[7]}\nПродолжительность дня: {s[8]}\n"
                                     f"Хорошего дня!"
                                 )
-            elif message.text == 'Курган':
-                r = "http://api.openweathermap.org/data/2.5/weather?q=kurgan&appid=12a7eeeda8c4becc9503870405c37f9e&units=metric"
-                r = requests.get(r)
-                data = r.json()
-                city = data["name"]
-                cur_weather = data["main"]["temp"]
-                weather_description = data["weather"][0]["main"]
-                humidity = data["main"]["humidity"]
-                pressure = data["main"]["pressure"]
-                wind = data["wind"]["speed"]
-                wd = "Посмотри в окно, не пойму что там за погода!"
-                sunrise_timestamp = datetime.fromtimestamp(data["sys"]["sunrise"])
-                sunset_timestamp = datetime.fromtimestamp(data["sys"]["sunset"])
-                length_of_the_day = datetime.fromtimestamp(data["sys"]["sunset"]) - datetime.fromtimestamp(
-                data["sys"]["sunrise"])
+            
+            elif message.text == 'Санкт-Петербург':
+                g = "Saint%20Petersburg"
+                s = get_weather(g)
                 bot.send_message(
                     message.chat.id, f"***{datetime.now().strftime('%Y-%m-%d %H:%M')}***\n"
-                                    f"Погода в городе: Курган\nТемпература: {cur_weather}C°\n"
-                                    f"Влажность: {humidity}%\nДавление: {pressure} мм.рт.ст\nВетер: {wind} м/с\n"
-                                    f"Восход солнца: {sunrise_timestamp}\nЗакат солнца: {sunset_timestamp}\nПродолжительность дня: {length_of_the_day}\n"
+                                    f"Погода в городе: Москва\nТемпература: {s[1]}C°\n"
+                                    f"Влажность: {s[3]}%\nДавление: {s[4]} мм.рт.ст\nВетер: {s[5]} м/с\n"
+                                    f"Восход солнца: {s[6]}\nЗакат солнца: {s[7]}\nПродолжительность дня: {s[8]}\n"
+                                    f"Хорошего дня!"
+                                )
+                
+            elif message.text == 'Курган':
+                g = "kurgan"
+                s = get_weather(g)
+                bot.send_message(
+                    message.chat.id, f"***{datetime.now().strftime('%Y-%m-%d %H:%M')}***\n"
+                                    f"Погода в городе: Москва\nТемпература: {s[1]}C°\n"
+                                    f"Влажность: {s[3]}%\nДавление: {s[4]} мм.рт.ст\nВетер: {s[5]} м/с\n"
+                                    f"Восход солнца: {s[6]}\nЗакат солнца: {s[7]}\nПродолжительность дня: {s[8]}\n"
                                     f"Хорошего дня!"
                                 )
             elif message.text == 'Дубаи':
-                r = "http://api.openweathermap.org/data/2.5/weather?q=dubai&appid=12a7eeeda8c4becc9503870405c37f9e&units=metric"
-                r = requests.get(r)
-                data = r.json()
-                city = data["name"]
-                cur_weather = data["main"]["temp"]
-                weather_description = data["weather"][0]["main"]
-                humidity = data["main"]["humidity"]
-                pressure = data["main"]["pressure"]
-                wind = data["wind"]["speed"]
-                wd = "Посмотри в окно, не пойму что там за погода!"
-                sunrise_timestamp = datetime.fromtimestamp(data["sys"]["sunrise"])
-                sunset_timestamp = datetime.fromtimestamp(data["sys"]["sunset"])
-                length_of_the_day = datetime.fromtimestamp(data["sys"]["sunset"]) - datetime.fromtimestamp(
-                data["sys"]["sunrise"])
+                g = "dubai"
+                s = get_weather(g)
                 bot.send_message(
                     message.chat.id, f"***{datetime.now().strftime('%Y-%m-%d %H:%M')}***\n"
-                                    f"Погода в городе: Дубаи\nТемпература: {cur_weather}C°\n"
-                                    f"Влажность: {humidity}%\nДавление: {pressure} мм.рт.ст\nВетер: {wind} м/с\n"
-                                    f"Восход солнца: {sunrise_timestamp}\nЗакат солнца: {sunset_timestamp}\nПродолжительность дня: {length_of_the_day}\n"
+                                    f"Погода в городе: Москва\nТемпература: {s[1]}C°\n"
+                                    f"Влажность: {s[3]}%\nДавление: {s[4]} мм.рт.ст\nВетер: {s[5]} м/с\n"
+                                    f"Восход солнца: {s[6]}\nЗакат солнца: {s[7]}\nПродолжительность дня: {s[8]}\n"
                                     f"Хорошего дня!"
                                 )
+               
     bot.polling()
 if __name__ == '__main__':
     #get_data()   
